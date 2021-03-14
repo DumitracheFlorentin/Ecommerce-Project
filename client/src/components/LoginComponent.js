4900029a7390edbc958e3be7fd2944ce94990c28
Florentin Dumitrache <dumitracheflorentincristian@gmail.com>
2021-03-14 19:13:30 +0200

    change nav depending on the token

--- client/src/components/LoginComponent.js
+++ client/src/components/LoginComponent.js
@@ -1,14 +1,17 @@
 import axios from "axios";
 import { useRef, useState } from "react";
+import { useHistory } from "react-router-dom";
 
 // Import files
 import NavbarComponent from "./NavbarComponent";
 
 // Import Bootstrap
-import { Container, Form, Button } from "react-bootstrap";
+import { Container, Form, Button, Alert } from "react-bootstrap";
 
 const LoginComponent = () => {
+  let history = useHistory();
   const [msg, setMsg] = useState();
+  const [succ, setSucc] = useState(false);
   const emailRef = useRef();
   const passwordRef = useRef();
 
@@ -23,10 +26,11 @@ const LoginComponent = () => {
       .then((res) => {
         if (res.data.msg) {
           setMsg(res.data.msg);
-          console.log(res.data);
         } else {
+          setMsg();
           localStorage.setItem("token", res.data.token);
-          console.log(res.data);
+          setSucc(true);
+          setTimeout(() => history.push("/"), 4000);
         }
       })
       .catch((err) => {
@@ -38,33 +42,53 @@ const LoginComponent = () => {
     <>
       <NavbarComponent />
       <Container className="preFormLogin">
-        <Form className="formLogin">
-          <Form.Group controlId="formGroupEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control
-              type="email"
-              placeholder="Enter email"
-              ref={emailRef}
-              required
-            />
-          </Form.Group>
-          <Form.Group controlId="formGroupPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control
-              type="password"
-              placeholder="Password"
-              ref={passwordRef}
-              required
-            />
-          </Form.Group>
-          <Button
-            variant="warning"
-            className="loginFormButton"
-            onClick={loginButtonHandler}
-          >
-            Log in
-          </Button>
-        </Form>
+        {msg && <Alert variant="danger">{msg}</Alert>}
+        {succ && (
+          <Alert variant="dark">
+            <Alert.Heading>Hey, nice to have you back</Alert.Heading>
+            <p>
+              Aww yeah, you successfully log in to your account. Be careful,
+              dont share to anyone your contact details. Our STAFF will never
+              ask for your email or password details.
+            </p>
+            <hr />
+            <p className="mb-0">
+              Enjoy our website and hope you will find what you are searching!
+            </p>
+            <p className="mb-0">
+              You will be redirected to the login page in 4 seconds!
+            </p>
+          </Alert>
+        )}
+        {!succ && (
+          <Form className="formLogin">
+            <Form.Group controlId="formGroupEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                ref={emailRef}
+                required
+              />
+            </Form.Group>
+            <Form.Group controlId="formGroupPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                ref={passwordRef}
+                required
+              />
+            </Form.Group>
+            <Button
+              variant="warning"
+              className="loginFormButton"
+              onClick={loginButtonHandler}
+            >
+              Log in
+            </Button>
+          </Form>
+        )}
       </Container>
     </>
   );
--- client/src/components/NavbarComponent.js
+++ client/src/components/NavbarComponent.js
@@ -1,10 +1,12 @@
-import { useHistory, Link } from "react-router-dom";
+import { useState } from "react";
 
 // Import Bootstrap
 import { LinkContainer } from "react-router-bootstrap";
 import { Nav, Navbar, Container } from "react-bootstrap";
 
 const NavbarComponent = () => {
+  const [token, setToken] = useState();
+
   return (
     <>
       <Navbar bg="dark" variant="dark">
@@ -17,9 +19,23 @@ const NavbarComponent = () => {
             <LinkContainer to="/cart">
               <Nav.Link>CART</Nav.Link>
             </LinkContainer>
-            <LinkContainer to="/register">
-              <Nav.Link>SIGN IN</Nav.Link>
-            </LinkContainer>
+
+            {!localStorage.getItem("token") ? (
+              <>
+                <LinkContainer to="/register">
+                  <Nav.Link>SIGN IN</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/login">
+                  <Nav.Link>LOG IN</Nav.Link>
+                </LinkContainer>
+              </>
+            ) : (
+              <>
+                <LinkContainer to="/account">
+                  <Nav.Link>My account</Nav.Link>
+                </LinkContainer>
+              </>
+            )}
           </Nav>
         </Container>
       </Navbar>
