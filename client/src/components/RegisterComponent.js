e4a8e0910e6767b471e30efd1d126e2ce95c7dcf
Florentin Dumitrache <dumitracheflorentincristian@gmail.com>
2021-03-14 11:57:09 +0200

    add alerts

--- client/src/components/RegisterComponent.js
+++ client/src/components/RegisterComponent.js
@@ -1,10 +1,16 @@
 import axios from "axios";
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import { useHistory } from "react-router-dom";
 
+// Import Bootstrap
 import { LinkContainer } from "react-router-bootstrap";
-import { Container, Form, Button } from "react-bootstrap";
+import { Container, Form, Button, Alert } from "react-bootstrap";
 
 const RegisterComponent = () => {
+  let history = useHistory();
+  const [resInfo, setResInfo] = useState(false);
+  const [msgInfo, setMsgInfo] = useState();
+
   const emailRef = useRef();
   const passwordRef = useRef();
   const firstnameRef = useRef();
@@ -12,7 +18,7 @@ const RegisterComponent = () => {
   const addressRef = useRef();
   const phoneRef = useRef();
 
-  const registerFromHandler = (e) => {
+  const RegFormHandler = (e) => {
     e.preventDefault();
 
     axios
@@ -24,83 +30,112 @@ const RegisterComponent = () => {
         address: addressRef.current.value,
         phone: phoneRef.current.value,
       })
-      .then((res) => console.log(res.data))
+      .then((res) => {
+        if (res.data.msg === "The account was created!") {
+          setResInfo(true);
+          setMsgInfo();
+
+          setTimeout(() => history.push("/login"), 5000);
+        } else {
+          setMsgInfo(res.data.msg);
+        }
+      })
       .catch((err) => console.log(err.message));
   };
 
   return (
     <Container className="preFormReg">
-      <Form className="formReg">
-        <Form.Group controlId="formGroupEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Enter email"
-            ref={emailRef}
-            required
-          />
-        </Form.Group>
-        <Form.Group controlId="formGroupPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Password"
-            ref={passwordRef}
-            required
-          />
-        </Form.Group>
-        <Form.Group controlId="formGroupFirstName">
-          <Form.Label>First Name</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter first name"
-            ref={firstnameRef}
-            required
-          />
-        </Form.Group>
-        <Form.Group controlId="formGroupLastName">
-          <Form.Label>Last Name</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter last name"
-            ref={lastnameRef}
-            required
-          />
-        </Form.Group>
-        <Form.Group controlId="formGroupAddress">
-          <Form.Label>Address</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter address"
-            ref={addressRef}
-            required
-          />
-        </Form.Group>
-        <Form.Group controlId="formGroupPhone">
-          <Form.Label>Phone</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter phone number"
-            ref={phoneRef}
-            required
-          />
-        </Form.Group>
-        <div className="buttonsGroup">
-          <Button
-            variant="warning"
-            className="regButton"
-            value="submit"
-            onClick={registerFromHandler}
-          >
-            Register
-          </Button>
-          <LinkContainer to="/login">
-            <Button variant="secondary" className="loginButton">
-              Sign In
+      {msgInfo !== undefined && <Alert variant="danger">{msgInfo}</Alert>}
+      {resInfo && (
+        <Alert variant="dark">
+          <Alert.Heading>Hey, nice to have you</Alert.Heading>
+          <p>
+            Aww yeah, you successfully register the account. Be careful, dont
+            share to anyone your contact details. Our STAFF will never ask for
+            your email or password details.
+          </p>
+          <hr />
+          <p className="mb-0">
+            Enjoy our website and hope you will find what you are searching!
+          </p>
+          <p className="mb-0">
+            You will be redirected to the login page in 4 seconds!
+          </p>
+        </Alert>
+      )}
+      {!resInfo && (
+        <Form className="formReg">
+          <Form.Group controlId="formGroupEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              ref={emailRef}
+              required
+            />
+          </Form.Group>
+          <Form.Group controlId="formGroupPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              ref={passwordRef}
+              required
+            />
+          </Form.Group>
+          <Form.Group controlId="formGroupFirstName">
+            <Form.Label>First Name</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter first name"
+              ref={firstnameRef}
+              required
+            />
+          </Form.Group>
+          <Form.Group controlId="formGroupLastName">
+            <Form.Label>Last Name</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter last name"
+              ref={lastnameRef}
+              required
+            />
+          </Form.Group>
+          <Form.Group controlId="formGroupAddress">
+            <Form.Label>Address</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter address"
+              ref={addressRef}
+              required
+            />
+          </Form.Group>
+          <Form.Group controlId="formGroupPhone">
+            <Form.Label>Phone</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter phone number"
+              ref={phoneRef}
+              required
+            />
+          </Form.Group>
+          <div className="buttonsGroup">
+            <Button
+              variant="warning"
+              className="regButton"
+              value="submit"
+              onClick={RegFormHandler}
+            >
+              Register
             </Button>
-          </LinkContainer>
-        </div>
-      </Form>
+            <LinkContainer to="/login">
+              <Button variant="secondary" className="loginButton">
+                Log in
+              </Button>
+            </LinkContainer>
+          </div>
+        </Form>
+      )}
     </Container>
   );
 };
