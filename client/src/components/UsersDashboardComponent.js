253a37f027ac93d727a28fb158d3d6a88cd18d40
Florentin Dumitrache <dumitracheflorentincristian@gmail.com>
2021-03-17 00:25:09 +0200

    finish user dashboard component

--- client/src/components/UsersDashboardComponent.js
+++ client/src/components/UsersDashboardComponent.js
@@ -33,6 +33,7 @@ const UsersDashboardComponent = () => {
   const lastnameRef = useRef();
   const addressRef = useRef();
   const phoneRef = useRef();
+  const adminRef = useRef();
 
   // useHistory
   let history = useHistory();
@@ -79,6 +80,28 @@ const UsersDashboardComponent = () => {
       })
       .catch((err) => console.log(err));
   };
+  const MessagePatchUserHandler = (id) => {
+    setId(id);
+    handleShow();
+    setPart("PatchUser");
+  };
+  const PatchUserHandler = (id) => {
+    axios
+      .patch(`http://localhost:5000/api/users/update/${id}`, {
+        password: passwordRef.current.value,
+        email: emailRef.current.value,
+        firstName: firstnameRef.current.value,
+        lastName: lastnameRef.current.value,
+        address: addressRef.current.value,
+        phone: phoneRef.current.value,
+        isAdmin: adminRef.current.value,
+      })
+      .then((res) => {
+        res.data.msg === "The account was updated!" && handleClose();
+        setCheckAlert(checkAlert + "updatedUser");
+      })
+      .catch((err) => console.log(err));
+  };
 
   // useEffect
   useEffect(() => {
@@ -119,25 +142,32 @@ const UsersDashboardComponent = () => {
           <Table className="tableProducts">
             <thead>
               <tr style={{ textAlign: "center" }}>
-                <th>First Name</th>
-                <th>Last Name</th>
+                <th>Name</th>
                 <th>Email</th>
                 <th>Address</th>
+                <th>Admin</th>
                 <th>Command</th>
               </tr>
             </thead>
             <tbody>
               {users &&
                 users.map((user) => {
+                  console.log(user);
                   return (
                     <tr style={{ textAlign: "center" }}>
-                      <td>{user.firstName}</td>
-                      <td>{user.lastName}</td>
+                      <td>
+                        {user.lastName} {user.firstName}
+                      </td>
                       <td>{user.email}</td>
                       <td>{user.address}</td>
+                      <td>{user.isAdmin ? "True" : "False"}</td>
                       <td>
                         <div>
-                          <Button variant="primary" className="mr-3">
+                          <Button
+                            variant="primary"
+                            className="mr-3"
+                            onClick={() => MessagePatchUserHandler(user._id)}
+                          >
                             Edit
                           </Button>
                           <Button
@@ -233,18 +263,87 @@ const UsersDashboardComponent = () => {
             </Button>
           </Modal.Footer>
         </Modal>
-      ) : part === "EditProduct" ? (
+      ) : part === "PatchUser" ? (
         <Modal show={show} onHide={handleClose}>
           <Modal.Header closeButton>
-            <Modal.Title>Edit Product</Modal.Title>
+            <Modal.Title>Edit User</Modal.Title>
           </Modal.Header>
-          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+          <Modal.Body>
+            <Form>
+              <Form.Row>
+                <Form.Group as={Col} controlId="formGridEmail">
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control
+                    type="email"
+                    placeholder="Enter email"
+                    ref={emailRef}
+                  />
+                </Form.Group>
+
+                <Form.Group as={Col} controlId="formGridPassword">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control
+                    type="password"
+                    placeholder="Password"
+                    ref={passwordRef}
+                  />
+                </Form.Group>
+              </Form.Row>
+
+              <Form.Row>
+                <Form.Group as={Col} controlId="formGridFirstName">
+                  <Form.Label>First Name</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="George"
+                    ref={firstnameRef}
+                  />
+                </Form.Group>
+
+                <Form.Group as={Col} controlId="formGridLastName">
+                  <Form.Label>Last Name</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Henken"
+                    ref={lastnameRef}
+                  />
+                </Form.Group>
+              </Form.Row>
+
+              <Form.Row>
+                <Form.Group as={Col} controlId="formGridAddress">
+                  <Form.Label>Address</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="1234 Main St"
+                    ref={addressRef}
+                  />
+                </Form.Group>
+
+                <Form.Group as={Col} controlId="formGridPhone">
+                  <Form.Label>Phone</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="0712231318"
+                    ref={phoneRef}
+                  />
+                </Form.Group>
+              </Form.Row>
+              <Form.Group controlId="formGridState">
+                <Form.Label>Admin</Form.Label>
+                <Form.Control as="select" defaultValue="false" ref={adminRef}>
+                  <option value="false">False</option>
+                  <option value="true">True</option>
+                </Form.Control>
+              </Form.Group>
+            </Form>
+          </Modal.Body>
           <Modal.Footer>
             <Button variant="secondary" onClick={handleClose}>
               Close
             </Button>
-            <Button variant="primary" onClick={handleClose}>
-              Save Changes
+            <Button variant="primary" onClick={() => PatchUserHandler(id)}>
+              Update
             </Button>
           </Modal.Footer>
         </Modal>
@@ -258,7 +357,9 @@ const UsersDashboardComponent = () => {
               Are you sure you want to delete this account?
             </Modal.Body>
             <Modal.Footer>
-              <Button variant="secondary">Close</Button>
+              <Button variant="secondary" onClick={handleClose}>
+                Close
+              </Button>
               <Button variant="primary" onClick={() => DeleteUserHandler(id)}>
                 Delete User
               </Button>
